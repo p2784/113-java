0bfbe3bb0b205e71410b575566d5b30b08e037f4
p2784 <q0075359@gmail.com>
2024-10-24 13:12:41 +0800

    HW1

--- 113-10-18/HW1/src/Time1.java
+++ 113-10-18/HW1/src/Time1.java
@@ -1,17 +1,80 @@
 public class Time1 {
-    //時(hour)
-    //分(minute)
-    //秒(second)
+    private int hour;   // 時
+    private int minute; // 分
+    private int second; // 秒
 
     //方法:建構子(設定時 分 秒的初始值，需檢查資料是否格式正確)
+    public Time1(int hour, int minute, int second) {
+        if (hour < 0 || hour >= 24) {
+            throw new IllegalArgumentException("Hour must be 0-23");
+        }
+        if (minute < 0 || minute >= 60) {
+            throw new IllegalArgumentException("Minute must be 0-59");
+        }
+        if (second < 0 || second >= 60) {
+            throw new IllegalArgumentException("Second must be 0-59");
+        }
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
 
-    //方法: 是需要加入get/set
+    // get/set 方法
+    public int getHour() {
+        return hour;
+    }
 
-    //方法: tick (時間增加一秒)
+    public void setHour(int hour) {
+        if (hour < 0 || hour >= 24) {
+            throw new IllegalArgumentException("Hour must be 0-23");
+        }
+        this.hour = hour;
+    }
 
-    //方法: printUniversal (以24小時制輸出完整的時間 14:05:32)
+    public int getMinute() {
+        return minute;
+    }
 
-    //方法: printStandard (以12小時制輸出完整的時間 2:05:32 PM)
+    public void setMinute(int minute) {
+        if (minute < 0 || minute >= 60) {
+            throw new IllegalArgumentException("Minute must be 0-59");
+        }
+        this.minute = minute;
+    }
 
+    public int getSecond() {
+        return second;
+    }
 
+    public void setSecond(int second) {
+        if (second < 0 || second >= 60) {
+            throw new IllegalArgumentException("Second must be 0-59");
+        }
+        this.second = second;
+    }
+
+    // tick 方法 (時間增加一秒)
+    public void tick() {
+        if (++second == 60) {
+            second = 0;
+            if (++minute == 60) {
+                minute = 0;
+                if (++hour == 24) {
+                    hour = 0;
+                }
+            }
+        }
+    }
+
+    // printUniversal 方法 (以24小時制輸出完整的時間 14:05:32)
+    public String printUniversal() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
+    // printStandard 方法 (以12小時制輸出完整的時間 2:05:32 PM)
+    public String printStandard() {
+        return String.format("%d:%02d:%02d %s",
+                ((hour == 0 || hour == 12) ? 12 : hour % 12),
+                minute, second, (hour < 12 ? "AM" : "PM"));
+    }
 }
--- 113-10-18/HW1/src/TimeTtest.java
+++ 113-10-18/HW1/src/TimeTtest.java
@@ -1,22 +1,46 @@
+import java.util.Scanner;
+
 public class TimeTtest {
     public static void main(String[] args) {
-        //Time1 time = new ....
+        Scanner scanner = new Scanner(System.in);
 
-        //呼叫printUniversal()
+        // 輸入時、分、秒
+        System.out.print("請輸入小時 (0-23): ");
+        int hour = scanner.nextInt();
+        System.out.print("請輸入分鐘 (0-59): ");
+        int minute = scanner.nextInt();
+        System.out.print("請輸入秒 (0-59): ");
+        int second = scanner.nextInt();
 
-        //呼叫printStandard()
+        // 建立 Time1 物件
+        Time1 time = new Time1(hour, minute, second);
 
-        //使用者輸入時間過了幾秒，用迴圈呼叫tick()
+        // 呼叫 printUniversal()
+        System.out.println("24小時制時間: " + time.printUniversal());
 
-        //呼叫printUniversal()
+        // 呼叫 printStandard()
+        System.out.println("12小時制時間: " + time.printStandard());
 
-        //呼叫printStandard()
+        // 使用者輸入時間過了幾秒，用迴圈呼叫 tick()
+        System.out.print("請輸入要增加的秒數: ");
+        int secondsToAdd = scanner.nextInt();
+        for (int i = 0; i < secondsToAdd; i++) {
+            time.tick();
+        }
 
-        /*Time1 time = new Time1(23, 59, 59);
+        // 呼叫 printUniversal()
+        System.out.println("增加秒數後的24小時制時間: " + time.printUniversal());
+
+        // 呼叫 printStandard()
+        System.out.println("增加秒數後的12小時制時間: " + time.printStandard());
+
+        scanner.close();
+    }
+}
+
+/*Time1 time = new Time1(23, 59, 59);
         System.out.println(time.printUniversal());
         System.out.println(time.printStandard());
         time.tick();
         System.out.println(time.printUniversal());
         System.out.println(time.printStandard());*/
-    }
-}
