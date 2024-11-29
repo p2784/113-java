d712a333f6c3d9032296fe8f81108f325705b49a
p2784 <q0075359@gmail.com>
2024-11-29 11:55:34 +0800

    113-11-29

---
+++ 113-11-29/InnerClass/src/Calculator.java
@@ -0,0 +1,39 @@
+public class Calculator {
+    class Operation {
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public int subtract(int a, int b) {
+            return a - b;
+        }
+
+        public int multiply(int a, int b) {
+            return a * b;
+        }
+
+        public int divide(int a, int b) {
+            return a / b;
+        }
+    }
+
+    public void performOperations(int a, String operator, int b) {
+        Operation operation = new Operation();
+        switch (operator) {
+            case "+":
+                System.out.println(operation.add(a, b));
+                break;
+            case "-":
+                System.out.println(operation.subtract(a, b));
+                break;
+            case "*":
+                System.out.println(operation.multiply(a, b));
+                break;
+            case "/":
+                System.out.println(operation.divide(a, b));
+                break;
+            default:
+                System.out.println("不支援此運算");
+        }
+    }
+}
