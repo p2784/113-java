4e6497eb3b27b23da9e0bcfb8499fcbda63e86ea
p2784 <q0075359@gmail.com>
2024-12-13 13:05:06 +0800

    113-12-13

---
+++ 113-12-13/ThrowException/src/FileReadTest.java
@@ -0,0 +1,12 @@
+
+public class FileReadTest {
+    public static void main(String[] args) {
+        try {
+            String text = FileUtil.read("cashcard1.txt");
+            System.out.println(text);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
